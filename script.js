2b870fc00d9ac4ba5cc4b6e7e44fd7e32772caaa
rafaelcastrobr <rafaelcastrobr@gmail.com>
2022-02-25 16:54:07 -0300

    CRUD pronto

---
+++ script.js
@@ -0,0 +1,73 @@
+const inputElement = document.querySelector('.new-task-input')
+const addTaskButton = document.querySelector('.new-task-button')
+const tasksContainer = document.querySelector('.tasks-container')
+
+const validateInput = () => inputElement.value.trim().length > 0;
+
+const handleAddTask = () => {
+  const inputIsValid = validateInput()
+
+  console.log(inputIsValid)
+
+  if(!inputIsValid) {
+    return inputElement.classList.add('error');
+  }
+
+  const taskItemContainer = document.createElement('div')
+  taskItemContainer.classList.add('task-item')
+
+  const taskContent = document.createElement('p')
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener('click', () => handClick(taskContent))
+
+  const deleteItem = document.createElement('i')
+  deleteItem.classList.add('far')
+  deleteItem.classList.add('fa-trash-alt')
+  deleteItem.classList.add('delete-item-taskbutton')
+
+  deleteItem.addEventListener('click', () => handleDeleteClick(taskItemContainer, taskContent))
+
+
+  taskItemContainer.appendChild(taskContent)
+  taskItemContainer.appendChild(deleteItem)
+  tasksContainer.appendChild(taskItemContainer)
+
+
+  inputElement.value = '';
+};
+
+const handClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskBeginClicked = task.firstChild.isSameNode(taskContent);
+    if(currentTaskBeginClicked) {
+      task.firstChild.classList.toggle('completed')
+    }
+  }
+}
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskBeginClicked = task.firstChild.isSameNode(taskContent);
+    if(currentTaskBeginClicked) {
+      taskItemContainer.remove();
+    }
+  }
+}
+
+const handleInputChange = () => {
+  const inputIsValid = validateInput();
+
+  if(inputIsValid) {
+    inputElement.classList.remove('error');
+  }
+}
+
+
+addTaskButton.addEventListener('click', () => handleAddTask());
+
+inputElement.addEventListener('change', () => handleInputChange())
